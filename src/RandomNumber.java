ffaa7c0a6af4acaee1aac1d9c26f678bc1c6beb1
SridharGNS <sridharguru2179@gmail.com>
2024-12-07 23:25:56 +0530

    DayFive

---
+++ src/RandomNumber.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        double n= Math.random();
+        double n2= Math.random();
+        System.out.println(n+""+" "+n2);
+
+    }
+}
