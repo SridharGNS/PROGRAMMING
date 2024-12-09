9283cb93bc47ac83c40f656c1e960aa36e8765d0
SridharGNS <sridharguru2179@gmail.com>
2024-12-09 22:19:33 +0530

    DaySeven

---
+++ src/PatternTwo.java
@@ -0,0 +1,17 @@
+public class PatternTwo {
+    public static void main(String[] args) {
+        for(int i=1;i<10;i++) {
+            for (int j = 10; j>1; j--)
+            {
+                if(j>i)
+                {
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print(("*"));
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
